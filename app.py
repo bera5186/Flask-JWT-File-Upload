936fe76cb8787b4df0e54a9b9f756dd117d318aa
bera5186 <rbasu611@gmail.com>
2020-03-17 21:15:35 +0530

    Added tests

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ from pymongo import MongoClient
 import bcrypt
 
 salt = bcrypt.gensalt(10)
-client = MongoClient('mongodb+srv://rahulbera21:rahul123@cluster0-9qlne.mongodb.net/test?retryWrites=true&w=majority')
+client = MongoClient(
+    "mongodb+srv://rahulbera21:rahul123@cluster0-9qlne.mongodb.net/test?retryWrites=true&w=majority"
+)
 db = client.flask_jwt_api_file_upload
 users = db.users
 
@@ -33,12 +35,12 @@ def token_required(f):
         token = session["token"]
 
         if not token:
-            return render_template('sucess.html', message = 'Token Missing')
+            return render_template("sucess.html", message="Token Missing")
         try:
             data = jwt.decode(token, app.config["SECRET_KEY"])
 
         except:
-            return render_template('sucess.html', message = 'Token Expired')
+            return render_template("sucess.html", message="Token Expired")
 
         return f(*args, **kwargs)
 
@@ -47,14 +49,14 @@ def token_required(f):
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
-    if request.method == 'POST':
+    if request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
 
         user = users.find_one({"email": email})
 
         if user:
-            return render_template('register.html', message = 'Email already Registered')
+            return render_template("register.html", message="Email already Registered")
 
         else:
             password_hash = bcrypt.hashpw(password.encode("UTF-8"), salt)
@@ -62,9 +64,10 @@ def signup():
 
             users.insert_one(user)
 
-            return render_template('register.html', message = 'Registration Completed')
+            return render_template("register.html", message="Registration Completed")
 
-    return render_template('register.html')
+    return render_template("register.html")
+
 
 @app.route("/")
 def home():
@@ -76,11 +79,11 @@ def home():
 @token_required
 def upload():
     if request.method == "POST":
-        file = request.files['file']
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
-        return render_template('sucess.html', message = file.filename)
-    
-
+        file = request.files["file"]
+        file.save(
+            os.path.join(app.config["UPLOAD_FOLDER"], secure_filename(file.filename))
+        )
+        return render_template("sucess.html", message=file.filename)
 
 
 @app.route("/token")
@@ -106,9 +109,9 @@ def getauthtoken():
                 app.config["SECRET_KEY"],
             )
             session["token"] = token
-            return render_template('token.html', token = token)
+            return render_template("token.html", token=token)
         else:
-            return render_template('token.html', token = "Invalid Credentials")
+            return render_template("token.html", token="Invalid Credentials")
 
     return render_template("token.html")
 
---
+++ test.py
@@ -0,0 +1,39 @@
+import unittest
+
+from app import app
+
+
+class BasicTestCase(unittest.TestCase):
+    def setup(self):
+        tester = app.test_client(self)
+        app.config["SECRET_KEY"] = "!@#$%^&*"
+        app.config["UPLOAD_FOLDER"] = "images/"
+        app.config["DEBUG"] = False
+
+    def test_main_page(self):
+        tester = app.test_client(self)
+        response = tester.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page(self):
+        tester = app.test_client(self)
+        response = tester.get("/signup")
+        self.assertEqual(response.status_code, 200)
+
+    def test_register(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            "/signup", data=dict(email="someuser@gmail.com", password="pass123")
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_token_generated(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            "/getauthtoken", data=dict(email="someuser@gmail.com", password="pass123")
+        )
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
